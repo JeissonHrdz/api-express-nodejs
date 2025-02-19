c19329c33d8acffad28dc10bc14df59cc510bf36
JeissonHrdz <jeissonhrdz186@gmail.com>
2025-02-19 11:17:16 -0500

    Creacion servidor HTTP y configuraciones

--- 1.http.js
+++ 1.http.js
@@ -1,2 +1,22 @@
-const http = require('node:http') 
-console.log('Hello World')
+const http = require('node:http')
+
+const desiredPort = process.env.PORT ?? 1234
+
+const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html charset=utf-8')
+  if (req.url === '/') {
+    res.statusCode = 200
+    res.end('Bienvenido a mi pagina de inicio')
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200
+    res.end('Contatos de mi pagina')
+  } else {
+    res.statusCode = 404
+    res.end('Pagina no encontrada')
+  }
+}
+const server = http.createServer(processRequest)
+
+server.listen(desiredPort, () => {
+  console.log(`server listening on port http://localhost:${desiredPort}`)
+})
